dc07c326a58e1829c0f1a3c442108540c5e2cbec
YiniXu9506 <xuyini@pingcap.com>
2020-05-27 20:00:35 +0800

    data: add missing data

---
+++ src/data/toolsLandingPage.js
@@ -0,0 +1,28 @@
+const toolsLandingPageData = [
+  {
+    type: 'TiDB Operator',
+    desc:
+      'TiDB 是 PingCAP 公司设计的开源分布式 HTAP (Hybrid Transactional and Analytical Processing) 数据库，结合了传统的 RDBMS 和 NoSQL 的最佳特性。',
+    link: '/tidb-in-kubernetes/v1.1',
+  },
+  {
+    type: 'DM',
+    desc:
+      'TiDB 是 PingCAP 公司设计的开源分布式 HTAP (Hybrid Transactional and Analytical Processing) 数据库，结合了传统的 RDBMS 和 NoSQL 的最佳特性。',
+    link: '/tidb-data-migration/v1.0',
+  },
+  {
+    type: 'TiDB Tools',
+    desc:
+      'TiDB 是 PingCAP 公司设计的开源分布式 HTAP (Hybrid Transactional and Analytical Processing) 数据库，结合了传统的 RDBMS 和 NoSQL 的最佳特性。',
+    link: '/tidb/v4.0/tools',
+  },
+  {
+    type: 'TiUP',
+    desc:
+      'TiDB 是 PingCAP 公司设计的开源分布式 HTAP (Hybrid Transactional and Analytical Processing) 数据库，结合了传统的 RDBMS 和 NoSQL 的最佳特性。',
+    link: '/tiup/v1.0',
+  },
+]
+
+export { toolsLandingPageData }
