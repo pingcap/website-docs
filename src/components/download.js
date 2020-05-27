b850150ade6bd4201131bdde831e87d06e6bf2ea
YiniXu9506 <xuyini@pingcap.com>
2020-05-27 19:23:48 +0800

    chore: add intl for download pdf

--- src/components/download.js
+++ src/components/download.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
 
 const Download = ({ downloadURL }) => {    
     return (
         <section>
             <div className="doc-download">
-                <a href={`https://download.pingcap.com/${downloadURL}`}>下载 PDF</a>
+                <a href={`https://download.pingcap.com/${downloadURL}`}>
+                    <FormattedMessage id="download-pdf" />
+                </a>
             </div>
         </section>
     )
