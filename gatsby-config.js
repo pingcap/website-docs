2f6fe30600321fc62f86cdc8ff1c9b62505a56c0
YiniXu9506 <xuyini@pingcap.com>
2020-05-29 13:37:50 +0800

    style: remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: 'UA-99991864-7',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
       },
     },
