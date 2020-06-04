507c136c196d24c80830a2ab5bc479256c9d781c
YiniXu9506 <xuyini@pingcap.com>
2020-06-04 16:41:38 +0800

    style: adjust code block style

--- src/components/search/result.js
+++ src/components/search/result.js
@@ -3,38 +3,44 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Result = ({ results }) => {
+  console.log('results', results)
   return (
     <div className="search-result">
-      <p className="counts">
-        <FormattedMessage
-          id="search.resultTips.counts"
-          values={{ counts: results.length }}
-        />
-      </p>
-      <div className="results">
-        {results.length > 0 &&
-          results.map((r) => (
-            <div key={r.objectID} className="item">
-              <a href={r.url}>
-                <div className="title is-5">{r.hierarchy.lvl0}</div>
-              </a>
-              <a href={r.url}>
-                <div className="url">{r.url}</div>
-              </a>
-              <div
-                className="content"
-                dangerouslySetInnerHTML={{
-                  __html:
-                    r._highlightResult.content.value.length > 500
-                      ? r._snippetResult.content.value
-                      : r._highlightResult.content.value,
-                }}
-              ></div>
-            </div>
-          ))}
-      </div>
+      {results && (
+        <>
+          <p className="counts">
+            <FormattedMessage
+              id="search.resultTips.counts"
+              values={{ counts: results.length }}
+            />
+          </p>
+          <div className="results">
+            {results.length > 0 &&
+              results.map((r) => (
+                <div key={r.objectID} className="item">
+                  <a href={r.url}>
+                    <div className="title is-5">{r.hierarchy.lvl0}</div>
+                  </a>
+                  <a href={r.url}>
+                    <div className="url">{r.url}</div>
+                  </a>
+                  <div
+                    className="content"
+                    dangerouslySetInnerHTML={{
+                      __html:
+                        r._highlightResult.content.value.length > 500
+                          ? r._snippetResult.content.value
+                          : r._highlightResult.content.value,
+                    }}
+                  ></div>
+                </div>
+              ))}
+          </div>
+        </>
+      )}
+
       <div className="no-result">
-        {results.length === 0 && (
+        {(results === null || (results && results.length === 0)) && (
           <>
             <p>
               <FormattedMessage id="search.resultTips.title" />
