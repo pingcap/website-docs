1fd37682e996852f8284fa05a2d2c488c14d1463
YiniXu9506 <xuyini@pingcap.com>
2020-06-16 21:48:45 +0800

    fix: add download branch

--- markdown-pages/cli.js
+++ markdown-pages/cli.js
@@ -46,6 +46,7 @@ function main(argv) {
           ref,
           path: path ? path : '',
         },
+        `${__dirname}/contents/en/docs-tidb/${ref}`,
         [
           () => createReplaceImagePathStream(DOCS_IMAGE_CDN_URL),
           () => createReplaceCopyableStream(),
