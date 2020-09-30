3afa5be4643424ac758a7243ecb26797bf8fe256
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2020-09-30 11:47:10 +0800

    chore: add latest commit message for each file (#113)

---
+++ src/components/gitCommitInfo.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react'
+import { FormattedMessage } from 'react-intl'
+import IntlLink from '../components/IntlLink'
+import axios from 'axios'
+
+const GitCommitInfo = ({ repoInfo, base, title }) => {
+  const { owner, repo, ref, pathPrefix } = repoInfo || {}
+  const [latestCommit, setLatestCommit] = useState(null)
+
+  async function fetchLatestCommit() {
+    try {
+      const res = (
+        await axios.get(
+          `https://api.github.com/repos/${owner}/${repo}/commits?path=${pathPrefix}${base}`
+        )
+      ).data[0]
+      setLatestCommit(res)
+    } catch (e) {
+      console.log('Fail to fetch lasted commit')
+      return
+    }
+  }
+
+  useEffect(() => {
+    fetchLatestCommit()
+  }, [])
+
+  return (
+    <div className="commit-info">
+      {latestCommit && (
+        <>
+          <IntlLink
+            to={`https://github.com/${owner}/${repo}/blob/${ref}/${pathPrefix}${base}`}
+            type="outBoundLink"
+          >
+            {title}
+          </IntlLink>{' '}
+          <FormattedMessage id="latestCommit" />{' '}
+          {latestCommit.commit.author.date}:{' '}
+          <IntlLink to={latestCommit.html_url} type="outBoundLink">
+            {latestCommit.commit.message.split('\n')[0]}
+          </IntlLink>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default GitCommitInfo
