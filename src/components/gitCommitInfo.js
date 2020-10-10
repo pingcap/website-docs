630d1db16ef1e38057f1918f4107c14c68ce3762
YiniXu9506 <xuyini@pingcap.com>
2020-10-10 11:36:12 +0800

    chore: decode html in docs search result

--- src/components/gitCommitInfo.js
+++ src/components/gitCommitInfo.js
@@ -7,23 +7,23 @@ const GitCommitInfo = ({ repoInfo, base, title }) => {
   const { owner, repo, ref, pathPrefix } = repoInfo || {}
   const [latestCommit, setLatestCommit] = useState(null)
 
-  async function fetchLatestCommit() {
-    try {
-      const res = (
-        await axios.get(
-          `https://api.github.com/repos/${owner}/${repo}/commits?path=${pathPrefix}${base}`
-        )
-      ).data[0]
-      setLatestCommit(res)
-    } catch (e) {
-      console.log('Fail to fetch lasted commit')
-      return
-    }
-  }
-
   useEffect(() => {
+    async function fetchLatestCommit() {
+      try {
+        const res = (
+          await axios.get(
+            `https://api.github.com/repos/${owner}/${repo}/commits?path=${pathPrefix}${base}`
+          )
+        ).data[0]
+        setLatestCommit(res)
+      } catch (e) {
+        console.log('Fail to fetch lasted commit')
+        return
+      }
+    }
+
     fetchLatestCommit()
-  }, [])
+  }, [owner, repo, pathPrefix, base])
 
   return (
     <div className="commit-info">
