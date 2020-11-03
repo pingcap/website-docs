b0e0fd48badb89211b5802f0217ec645948711bd
YiniXu9506 <xuyini@pingcap.com>
2020-11-03 17:44:23 +0800

    chore: optimize user feedback

--- src/components/userFeedback.js
+++ src/components/userFeedback.js
@@ -7,12 +7,11 @@ import { FormattedMessage } from 'react-intl'
 import { trackCustomEvent } from 'gatsby-plugin-google-analytics'
 
 const UserFeedback = ({ title, locale }) => {
-  const feedbackBodyRef = useRef(null)
   const feedbackCloseRef = useRef(null)
-  const [showFollowUp, setShowFollowUp] = useState(false)
-  const [showYesFollowUp, setShowYesFollowUp] = useState(false)
+  const [showFeedbackBody, setShowFeedbackBody] = useState(false)
+  const [showYesFollowUp, setShowYesFollowUp] = useState('unset')
 
-  const setDocHelpful = (docTitle, isHelpful) => {
+  const setDocHelpful = (docTitle, isHelpful) => () => {
     trackCustomEvent({
       category: isHelpful ? `doc-${locale}-useful` : `doc-${locale}-useless`,
       action: 'click',
@@ -20,25 +19,24 @@ const UserFeedback = ({ title, locale }) => {
       transport: 'beacon',
     })
 
-    setShowFollowUp(true)
-
     if (isHelpful) {
-      setShowYesFollowUp(true)
+      setShowYesFollowUp('show')
+    } else {
+      setShowYesFollowUp('hide')
     }
   }
 
   const showThumbs = () => {
     if (!feedbackCloseRef.current.classList.contains('show-feedback-close')) {
-      feedbackBodyRef.current.classList.add('show-feedback-body')
+      setShowFeedbackBody(true)
       feedbackCloseRef.current.classList.add('show-feedback-close')
     }
   }
 
   const closeFeedback = () => {
     if (feedbackCloseRef.current.classList.contains('show-feedback-close')) {
-      setShowFollowUp(false)
-      setShowYesFollowUp(false)
-      feedbackBodyRef.current.classList.remove('show-feedback-body')
+      setShowFeedbackBody(false)
+      setShowYesFollowUp('unset')
       feedbackCloseRef.current.classList.remove('show-feedback-close')
     }
   }
@@ -64,48 +62,50 @@ const UserFeedback = ({ title, locale }) => {
           x
         </div>
       </div>
-      <div className="feedback-body" ref={feedbackBodyRef}>
-        {!showFollowUp && (
-          <div className="thumbs">
-            <div
-              role="button"
-              tabIndex={0}
-              className="thumb thumb-up"
-              onClick={() => setDocHelpful(title, true)}
-              onKeyDown={() => setDocHelpful(title, true)}
-            >
-              <FormattedMessage id="docHelpful.thumbUp" />
+      {showFeedbackBody && (
+        <div className="feedback-body">
+          {showYesFollowUp === 'unset' && (
+            <div className="thumbs">
+              <div
+                role="button"
+                tabIndex={0}
+                className="thumb thumb-up"
+                onClick={setDocHelpful(title, true)}
+                onKeyDown={setDocHelpful(title, true)}
+              >
+                <FormattedMessage id="docHelpful.thumbUp" />
+              </div>
+              <div
+                role="button"
+                tabIndex={0}
+                className="thumb thumb-down"
+                onClick={setDocHelpful(title, false)}
+                onKeyDown={setDocHelpful(title, false)}
+              >
+                <FormattedMessage id="docHelpful.thumbDown" />
+              </div>
             </div>
-            <div
-              role="button"
-              tabIndex={0}
-              className="thumb thumb-down"
-              onClick={() => setDocHelpful(title, false)}
-              onKeyDown={() => setDocHelpful(title, false)}
-            >
-              <FormattedMessage id="docHelpful.thumbDown" />
-            </div>
-          </div>
-        )}
+          )}
 
-        {showFollowUp && (
-          <div className="feedback-form">
-            <HubspotForm
-              portalId="4466002"
-              formId={`${
-                showYesFollowUp
-                  ? locale === 'en'
-                    ? 'c955b3db-740a-4f96-9d2b-011e2cd80ad6'
-                    : 'caf4026d-e3a0-4285-8f80-fcdee324f50d'
-                  : locale === 'en'
-                  ? '3c501775-c64d-4a9e-898b-7efef630bbf4'
-                  : '4bf44ac7-4104-4eca-a57c-4dd9e5cc87b9'
-              }`}
-              loading={<Loading wholeSreen={false} />}
-            />
-          </div>
-        )}
-      </div>
+          {showYesFollowUp !== 'unset' && (
+            <div className="feedback-form">
+              <HubspotForm
+                portalId="4466002"
+                formId={`${
+                  showYesFollowUp === 'show'
+                    ? locale === 'en'
+                      ? 'c955b3db-740a-4f96-9d2b-011e2cd80ad6'
+                      : 'caf4026d-e3a0-4285-8f80-fcdee324f50d'
+                    : locale === 'en'
+                    ? '3c501775-c64d-4a9e-898b-7efef630bbf4'
+                    : '4bf44ac7-4104-4eca-a57c-4dd9e5cc87b9'
+                }`}
+                loading={<Loading wholeSreen={false} />}
+              />
+            </div>
+          )}
+        </div>
+      )}
     </section>
   )
 }
