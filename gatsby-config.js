4a74e3d790dda667dbf02217e9ef4f8e3a5648dd
kennytm <kennytm@gmail.com>
2020-11-28 22:28:33 +0800

    fix: fix build failure

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
             },
           },
         ],
-        remarkPlugins: [remarkSyntaxDiagram],
+        remarkPlugins: [() => remarkSyntaxDiagram],
       },
     },
     {
