61b07e62f173d6af4b7c7097e0ecaf846343c4cf
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2020-12-31 11:40:28 +0800

    chore: add docs/docs-cn release-5.0 (#139)

--- scripts/get-latest-commits.sh
+++ scripts/get-latest-commits.sh
@@ -5,8 +5,8 @@ GITHUB_AUTHORIZATION_TOKEN=$1
 
 # repos in pingcap owner
 docs_pingcap_repos=(docs-cn docs docs-dm docs-tidb-operator dev-guide)
-docs_cn=(master release-4.0 release-3.1 release-3.0 release-2.1)
-docs=(master release-4.0 release-3.1 release-3.0 release-2.1)
+docs_cn=(master release-5.0 release-4.0 release-3.1 release-3.0 release-2.1)
+docs=(master release-5.0 release-4.0 release-3.1 release-3.0 release-2.1)
 docs_dm=(master release-1.0 release-2.0)
 docs_tidb_operator=(master release-1.1 release-1.0)
 dev_guide=(master)
