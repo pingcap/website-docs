b1bb8332b43a4851c748a46acfeaf499443cd567
YiniXu9506 <xuyini@pingcap.com>
2020-12-31 11:05:49 +0800

    chore: add docs/docs-cn release-5.0

--- scripts/download-docs-tidb-en.sh
+++ scripts/download-docs-tidb-en.sh
@@ -1,4 +1,5 @@
 yarn download:docs-tidb:en
+yarn download:docs-tidb:en --ref release-5.0
 yarn download:docs-tidb:en --ref release-4.0
 yarn download:docs-tidb:en --ref release-3.1
 yarn download:docs-tidb:en --ref release-3.0
