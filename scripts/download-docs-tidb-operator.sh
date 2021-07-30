696d4ab73859e933c46b3d2588d5610346aab5fd
YiniXu9506 <xuyini@pingcap.com>
2021-07-30 13:11:00 +0800

    chore: add tidb operator 1.2

--- scripts/download-docs-tidb-operator.sh
+++ scripts/download-docs-tidb-operator.sh
@@ -1,6 +1,8 @@
 yarn download:docs-tidb-operator en
 yarn download:docs-tidb-operator en release-1.0
 yarn download:docs-tidb-operator en release-1.1
+yarn download:docs-tidb-operator en release-1.2
 yarn download:docs-tidb-operator zh
 yarn download:docs-tidb-operator zh release-1.0
 yarn download:docs-tidb-operator zh release-1.1
+yarn download:docs-tidb-operator zh release-1.2
