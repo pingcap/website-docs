1afdccac6ebbedd7fe6ac57ef2e52e08c89aef94
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2021-07-30 12:07:30 +0800

    chore: add operator 1.2 (#168)

--- scripts/download-docs-tidb-operator-zh.sh
+++ scripts/download-docs-tidb-operator-zh.sh
@@ -1,3 +1,4 @@
 yarn download:docs-tidb-operator zh
 yarn download:docs-tidb-operator zh release-1.0
 yarn download:docs-tidb-operator zh release-1.1
+yarn download:docs-tidb-operator zh release-1.2
