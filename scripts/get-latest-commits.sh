ac172f6481e3e9adecfc7543c577331081c59a8f
Yue Yang <g1enyy0ung@gmail.com>
2021-10-20 14:16:47 +0800

    chore: update get-latest-commits
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- scripts/get-latest-commits.sh
+++ scripts/get-latest-commits.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
-DIR=$(dirname "$0")
-echo $DIR
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+CACHE_DIR="$DIR/../markdown-pages"
 GITHUB_AUTHORIZATION_TOKEN=$1
 
-# repos in pingcap owner
-docs_pingcap_repos=(docs-cn docs docs-dm docs-tidb-operator dev-guide)
-docs_cn=(master release-5.2 release-5.1 release-5.0 release-4.0 release-3.1 release-3.0 release-2.1)
+# repos in pingcap
+repos=(docs docs-cn docs-dm docs-tidb-operator dev-guide)
 docs=(master release-5.2 release-5.1 release-5.0 release-4.0 release-3.1 release-3.0 release-2.1)
+docs_cn=(master release-5.2 release-5.1 release-5.0 release-4.0 release-3.1 release-3.0 release-2.1)
 docs_dm=(master release-1.0 release-2.0)
 docs_tidb_operator=(master release-1.1 release-1.0 release-1.2)
 dev_guide=(master)
 
-# repos in tidbcloud owner
-docs_tidbcloud_repos=(dbaas-docs)
-dbaas_docs=(master)
-
-for docs_repo in "${docs_pingcap_repos[@]}"
+for repo in ${repos[@]}
 do
-    docs_vers="${docs_repo//-/_}"
-    versions=${docs_vers}[@]
-    for v in "${!versions}"
-    do
-       curl -H "Authorization: token $GITHUB_AUTHORIZATION_TOKEN" -s "https://api.github.com/repos/pingcap/$docs_repo/commits/$v"|\
-         python3 -c "import sys,json; print(json.load(sys.stdin)['sha'])" > $DIR/../markdown-pages/$docs_repo-$v-hash.lock
-        echo "$docs_repo-$v-hash.lock"
-        cat $DIR/../markdown-pages/$docs_repo-$v-hash.lock
-    done
+  repo_to_index="${repo//-/_}"
+  versions=$repo_to_index[@]
+
+  for v in ${!versions}
+  do
+    git ls-remote https://github.com/pingcap/$repo.git refs/heads/$v | awk '{ print $1 }' > $CACHE_DIR/$repo-$v-hash.lock
+    echo "$repo-$v-hash.lock" $(cat $CACHE_DIR/$repo-$v-hash.lock)
+  done
 done
 
-for docs_repo in "${docs_tidbcloud_repos[@]}"
+# repos in tidbcloud
+tidbcloud=(dbaas-docs)
+dbaas_docs=(master)
+
+for repo in ${tidbcloud[@]}
 do
-    docs_vers="${docs_repo//-/_}"
-    versions=${docs_vers}[@]
-    for v in "${!versions}"
-    do
-       curl -H "Authorization: token $GITHUB_AUTHORIZATION_TOKEN" -s "https://api.github.com/repos/tidbcloud/$docs_repo/commits/$v"|\
-         python3 -c "import sys,json; print(json.load(sys.stdin)['sha'])" > $DIR/../markdown-pages/$docs_repo-$v-hash.lock
-        echo "$docs_repo-$v-hash.lock"
-        cat $DIR/../markdown-pages/$docs_repo-$v-hash.lock
-    done
+  repo_to_index="${repo//-/_}"
+  versions=$repo_to_index[@]
+
+  for v in ${!versions}
+  do
+    $(curl -s -H "Authorization: token $GITHUB_AUTHORIZATION_TOKEN" \
+    -H "Accept: application/vnd.github.VERSION.sha" \
+    "https://api.github.com/repos/tidbcloud/$repo/commits/$v") > $CACHE_DIR/$repo-$v-hash.lock
+    echo "$repo-$v-hash.lock" $(cat $CACHE_DIR/$repo-$v-hash.lock)
+  done
 done
