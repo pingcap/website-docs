3403671b788c81ee220495cda3e598a62a8c258a
Yue Yang <g1enyy0ung@gmail.com>
2021-12-02 16:45:18 +0800

    chore: sync scripts
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- scripts/download-tidb-en.sh
+++ scripts/download-tidb-en.sh
@@ -1,5 +1,7 @@
 yarn download:tidb:en
+yarn download:tidb:en --ref release-5.3
 yarn download:tidb:en --ref release-5.2
+yarn download:tidb:en --ref release-5.1
 yarn download:tidb:en --ref release-5.0
 yarn download:tidb:en --ref release-4.0
 yarn download:tidb:en --ref release-3.1
