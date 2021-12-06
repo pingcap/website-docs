644fff3fccef0e429d3f6318f8f8571c9d00ec38
Yue Yang <g1enyy0ung@gmail.com>
2021-12-06 17:40:15 +0800

    fix: reduce bundle size by clear inline styles
    Signed-off-by: Yue Yang <g1enyy0ung@gmail.com>

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,19 @@
 export { default as wrapRootElement } from './src/state/wrap-with-provider'
+
+// https://github.com/gatsbyjs/gatsby/issues/1526
+export const onPreRenderHTML = ({ getHeadComponents }) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return
+  }
+
+  getHeadComponents().forEach(el => {
+    if (el.type === 'style' && el.props['data-href']) {
+      el.type = 'link'
+      el.props['href'] = el.props['data-href']
+      el.props['rel'] = 'stylesheet'
+
+      delete el.props['data-href']
+      delete el.props['dangerouslySetInnerHTML']
+    }
+  })
+}
