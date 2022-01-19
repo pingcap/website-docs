3e6ac6a9a1d05a6a62fa97136b534ead2e1138cf
Austaras <austaras@outlook.com>
2022-01-19 10:52:05 +0800

    test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -17,3 +17,18 @@ export const onPreRenderHTML = ({ getHeadComponents }) => {
     }
   })
 }
+
+// TODO: remove after we staticize everything
+const script = `if (Promise == null || Promise.allSettled == null) {
+  const div = document.createElement('div')
+  div.classList.add('notification', 'is-danger', 'is-light')
+  div.style = 'position: fixed; top: 0; left: 0; width: 100%;text-align:center; z-index: 9999'
+  div.innerText = 'Your current browser is not supported, please use latest Chrome or Firefox'
+  document.body.appendChild(div)
+}`
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script key="deprecated" dangerouslySetInnerHTML={{ __html: script }} />,
+  ])
+}
