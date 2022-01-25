35ce3bed04130ca452e6a4acd2a98943a391e374
Austaras <austaras@outlook.com>
2022-01-25 12:02:48 +0800

    build: remove console.log

--- src/components/shortcodes/TabsPanel.tsx
+++ src/components/shortcodes/TabsPanel.tsx
@@ -1,14 +1,11 @@
 import { tabsPanel, letterBtn } from './tabs-panel.module.scss'
 
-export const TabsPanel = ({ letters }: { letters: string }) => {
-  console.log(letters)
-  return (
-    <div className={tabsPanel}>
-      {letters.split('').map(letter => (
-        <a href={`#${letter}`} key={letter}>
-          <div className={letterBtn}>{letter}</div>
-        </a>
-      ))}
-    </div>
-  )
-}
+export const TabsPanel = ({ letters }: { letters: string }) => (
+  <div className={tabsPanel}>
+    {letters.split('').map(letter => (
+      <a href={`#${letter}`} key={letter}>
+        <div className={letterBtn}>{letter}</div>
+      </a>
+    ))}
+  </div>
+)
