94959d9e442bf338c881279c87b3d906d6c8ec4e
Austaras <austaras@outlook.com>
2022-02-11 16:43:42 +0800

    1.3

--- scripts/download-tidb-operator-en.sh
+++ scripts/download-tidb-operator-en.sh
@@ -1,4 +1,5 @@
 yarn download:tidb-operator en
+yarn download:tidb-operator en release-1.3
 yarn download:tidb-operator en release-1.2
 yarn download:tidb-operator en release-1.1
 yarn download:tidb-operator en release-1.0
