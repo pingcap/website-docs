ec5b3e145acb41bdeecaab9b3ab5d77b5011f7f3
Austaras <austaras@outlook.com>
2022-02-15 16:19:39 +0800

    mod: remove embed youtube video

--- src/components/shortcodes/index.ts
+++ src/components/shortcodes/index.ts
@@ -1,7 +1,6 @@
 export { Warning, Important, Note, Tip } from './Message'
 
 export { ColumnTitle } from './ColumnTitle'
-export { EmbedYouTube } from './EmbedYouTube'
 export { NavColumn, NavColumns } from './NavColumn'
 export { SimpleTab } from './SimpleTab'
 export { SyntaxDiagram } from './SyntaxDiagram'
