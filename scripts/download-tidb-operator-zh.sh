aeaa342b4c8709ce02318262b85e47f047e829f1
Austaras <austaras@outlook.com>
2022-02-15 18:19:49 +0800

    ci: add tidb operators 1.3

--- scripts/download-tidb-operator-zh.sh
+++ scripts/download-tidb-operator-zh.sh
@@ -1,4 +1,5 @@
 yarn download:tidb-operator zh
+yarn download:tidb-operator zh release-1.3
 yarn download:tidb-operator zh release-1.2
 yarn download:tidb-operator zh release-1.1
 yarn download:tidb-operator zh release-1.0
