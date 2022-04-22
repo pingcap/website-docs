04930301b71e74f223bb5c2c41ab65c0daa49b8c
czhen <56986964+shczhen@users.noreply.github.com>
2022-04-22 13:31:18 +0800

    support custom-id

--- src/doc/comp/Toc.tsx
+++ src/doc/comp/Toc.tsx
@@ -6,17 +6,37 @@ interface Props {
 
 const versionMark = /<span class="version-mark">.*<\/span>/
 
-export const Toc = ({ data }: Props) => (
-  <ul>
-    {data.map((item, i) =>
-      item.url ? (
-        <li key={item.url}>
-          <a href={item.url}>{item.title.replace(versionMark, '')}</a>
-          {item.items && <Toc data={item.items} />}
-        </li>
-      ) : (
-        <li key={`item${i}`}>{item.items && <Toc data={item.items} />}</li>
+export const Toc = ({ data }: Props) => {
+  // Support {#custom-id}
+  // Now `#origin-id {#custom-id}` will be transformed to `#custom-id`
+  const transformCustomId = (
+    label: string,
+    anchor: string
+  ): { label: string; anchor: string } => {
+    const customIdMatches = label.match(/(.+) *\{(#.+)\}$/)
+    if (customIdMatches?.length) {
+      const [, newLabel, newAnchor] = customIdMatches
+      return { label: newLabel, anchor: newAnchor }
+    }
+    return { label, anchor }
+  }
+
+  const renderTocItem = (item: TableOfContent): JSX.Element => {
+    const { url, title, items } = item
+    if (url) {
+      const { label: newLabel, anchor: newAnchor } = transformCustomId(
+        title,
+        url
       )
-    )}
-  </ul>
-)
+      return (
+        <li key={url}>
+          <a href={newAnchor}>{newLabel.replace(versionMark, '')}</a>
+          {items && <Toc data={items} />}
+        </li>
+      )
+    }
+    return <li key={`item-${title}-${url}`}>{items && <Toc data={items} />}</li>
+  }
+
+  return <ul>{data.map((item, i) => renderTocItem(item))}</ul>
+}
