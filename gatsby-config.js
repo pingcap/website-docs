ee78c3b679ae453380851dbe9c3750af3aaf2219
czhen <56986964+shczhen@users.noreply.github.com>
2022-04-26 11:47:10 +0800

    enable gtm

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,35 +13,19 @@ module.exports = {
   jsxRuntime: 'automatic',
   plugins: [
     {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-WQG8Z75',
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: 'gatsby' },
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-99991864-7',
         head: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          'G-BQ970DC3WX', // Google Analytics / GA
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: 'GTM-WQG8Z75',
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ['/preview/**', '/do-not-track/me/too/'],
-        },
       },
     },
     `gatsby-plugin-root-import`,
