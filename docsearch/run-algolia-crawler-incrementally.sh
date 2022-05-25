98bf19df351d03cb970d54d4c89b7a292fc949d5
czhen <56986964+shczhen@users.noreply.github.com>
2022-05-25 19:17:36 +0800

    update workflow

--- docsearch/run-algolia-crawler-incrementally.sh
+++ docsearch/run-algolia-crawler-incrementally.sh
@@ -58,3 +58,4 @@ docker run --rm --env-file=.env -e "CONFIG=$(cat $1/algolia_configs/en-appdev-de
 # crawl en tidbcloud
 docker run --rm --env-file=.env -e "CONFIG=$(cat $1/algolia_configs/en-tidbcloud.json | jq -r tostring)" -e  "ISINCREMENTAL=True" -v $1/algolia_configs:/data $DOCKER_REGISTRY/algolia-docsearch-scraper-incremental:v0.2
 
+echo "Incrementally crawl finished at: $now"
