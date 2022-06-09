7f8bce069b35c4933143a2ac9b66f4f9fe94bc4e
czhen <56986964+shczhen@users.noreply.github.com>
2022-06-09 10:42:14 +0800

    Update CustomContent.tsx (#257)

--- src/components/shortcodes/CustomContent.tsx
+++ src/components/shortcodes/CustomContent.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
 
 export const CustomContent = (props: { platform: string; children: any }) => {
+  // type platform = "tidb" | "tidb-cloud"
   const { platform: pageType, children } = props
   const [shouldDisplay, setShouldDisplay] = useState(true)
   useEffect(() => {
     const currentPath = window?.location?.pathname || ''
-    if (currentPath.includes(`/${pageType}/`)) {
+    if (currentPath.includes(`/${pageType.replace('-', '')}/`)) {
       setShouldDisplay(true)
     } else {
       setShouldDisplay(false)
