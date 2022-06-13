978df6a0d590be84a02237919c5a7f6f2258d230
czhen <56986964+shczhen@users.noreply.github.com>
2022-06-13 14:05:53 +0800

    temp modify siteUrl for algolia index build

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,8 @@ module.exports = {
     description:
       'Browse documentation about TiDB and its ecosystem, including TiDB Operator, TiDB Data Migration, Database Tools, TiUP, etc.',
     author: '@PingCAP',
-    siteUrl: 'https://docs.pingcap.com',
+    // siteUrl: 'https://docs.pingcap.com',
+    siteUrl: 'https://docs-ja-preview.netlify.app',
   },
   jsxRuntime: 'automatic',
   plugins: [
