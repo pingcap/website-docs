8d73c3ec6d02fddff3af0b70ed7678a0651ba02d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 17:28:00 +0800

    Delete src directory

--- src/components/shortcodes/index.ts
+++
@@ -1,9 +0,0 @@
-export { Warning, Important, Note, Tip } from './Message'
-
-export { ColumnTitle } from './ColumnTitle'
-export { NavColumn, NavColumns } from './NavColumn'
-export { SimpleTab } from './SimpleTab'
-export { SyntaxDiagram } from './SyntaxDiagram'
-export { TabsPanel } from './TabsPanel'
-export { WithCopy } from './WithCopy'
-export { CustomContent } from './CustomContent'
