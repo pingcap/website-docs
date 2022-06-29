e083a146d1796476fdec508a7d47b58f1331209f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 17:27:33 +0800

    Delete scripts directory

--- scripts/download-dm.sh
+++
@@ -1,4 +0,0 @@
-DIR=$(cd $(dirname $0) && pwd)
-
-sh $DIR/download-dm-en.sh
-sh $DIR/download-dm-zh.sh
--- scripts/download-tidb-ja.sh
+++
@@ -1,2 +0,0 @@
-yarn download:tidb:ja --ref i18n-ja-release-6.1
-yarn download:tidb:ja --ref i18n-ja-release-5.4
