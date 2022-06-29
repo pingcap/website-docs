7062ef0590c3ca97948a2f341cfa0411fc8c1951
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 17:38:31 +0800

    Delete gatsby-browser.js

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-import { pageView } from './scripts/track'
-
-export { default as wrapRootElement } from './src/state/wrap-with-provider'
-
-export const onRouteUpdate = () => {
-  if (process.env.NODE_ENV === 'production') {
-    pageView()
-  }
-}
