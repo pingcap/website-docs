16f17c12ec150c6d9d21d7440bd3881d357b0365
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-29 17:19:08 +0800

    Delete scripts directory

--- scripts/download-dm-zh.sh
+++
@@ -1,3 +0,0 @@
-yarn download:dm zh release-5.3
-yarn download:dm zh release-2.0
-yarn download:dm zh release-1.0
--- scripts/download-tidb-operator-zh.sh
+++
@@ -1,5 +0,0 @@
-yarn download:tidb-operator zh
-yarn download:tidb-operator zh release-1.3
-yarn download:tidb-operator zh release-1.2
-yarn download:tidb-operator zh release-1.1
-yarn download:tidb-operator zh release-1.0
