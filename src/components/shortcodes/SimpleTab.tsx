a7b4dae0d5a0911ba868ca7a5747db867e4cde83
xuliwenwenwen <liwen.xu@pingcap.com>
2022-07-13 18:29:22 +0800

    refactor: simple-tab component
    Signed-off-by: xuliwenwenwen <liwen.xu@pingcap.com>

--- src/components/shortcodes/SimpleTab.tsx
+++ src/components/shortcodes/SimpleTab.tsx
@@ -1,52 +1,44 @@
 import { tabs, active, normal, hidden } from './simple-tab.module.scss'
 
-import { ReactElement, useEffect, useState } from 'react'
-import { useLocation } from '@reach/router'
+import { ReactElement, useState } from 'react'
 import clsx from 'clsx'
 
 export function SimpleTab({
   children,
 }: {
-  children: ReactElement<{ label: string; href?: string; children: ReactElement[] }>[]
+  children: ReactElement<{
+    label: string
+    href?: string
+    children: ReactElement[]
+  }>[]
 }) {
-  const location = useLocation()
   const [activeTab, setActiveTab] = useState(0)
-  
-  useEffect(() => {
-    let active = children.findIndex(
-      child => {
-        const activeJudge: string = child.props?.href || child.props.label;
-        return activeJudge === decodeURIComponent(location.hash.slice(1))
-      }
-    )
-    if (active === -1) {
-      active = 0
-    }
-    setActiveTab(active)
-  }, [location.hash, children])
+
+  const handleActiveTabChange = (newValue: number) => {
+    setActiveTab(newValue)
+  }
 
   return (
     <>
       <ul className={tabs}>
         {children.map((child, index) => {
-          const id: string = child.props?.href || child.props.label;
+          const id: string = child.props?.href || child.props.label
           return (
-            <li key={id} id={id}>
-              <a
-                href={`#${id}`}
-                className={activeTab === index ? active : normal}>
-                {child.props.label}
-              </a>
+            <li
+              key={id}
+              className={activeTab === index ? active : normal}
+              onClick={() =>
+                activeTab !== index && handleActiveTabChange(index)
+              }>
+              {child.props.label}
             </li>
           )
         })}
       </ul>
       {children.map((child, index) => {
-        const id: string = child.props?.href || child.props.label;
+        const id: string = child.props?.href || child.props.label
         return (
-          <div
-            key={id}
-            className={clsx(activeTab !== index && hidden)}>
+          <div key={id} className={clsx(activeTab !== index && hidden)}>
             {child.props.children}
           </div>
         )
