b516acd41d90cb5ee00bc42d16331f0e6f749676
czhen <56986964+shczhen@users.noreply.github.com>
2022-07-26 10:02:17 +0800

    cloud openapi doc page (#308)

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,11 +2,12 @@ require('ts-node').register({ transpileOnly: true })
 
 const path = require('path')
 
-const { createDocs } = require('./gatsby/create-pages')
+const { createDocs, createCloudAPIReference } = require('./gatsby/create-pages')
 const { createExtraType } = require('./gatsby/create-types')
 
 exports.createPages = async ({ graphql, actions }) => {
   await createDocs({ graphql, actions })
+  await createCloudAPIReference({ graphql, actions })
   actions.createPage({
     path: '/search',
     component: path.resolve(__dirname, './src/search/index.tsx'),
