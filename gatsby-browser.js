db8b15b4928f65455fdbde6de63e64b234272751
czhen <56986964+shczhen@users.noreply.github.com>
2022-09-02 15:12:59 +0800

    update console msg

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,10 +13,10 @@ export const onClientEntry = () => {
                                                                                              
   =====================================
   Project        : pingcap/website-docs
-  Build Time     : ${process.env.BUILD_DATE}
+  Build Time     : ${process.env.GATSBY_DOC_BUILD_DATE}
   Env            : ${process.env.NODE_ENV}
-  GIT Tag        : ${process.env.BUILD_GIT_SHA}
-  Docs Staging   : ${process.env.BUILD_SUBMODULE_SHA}
+  GIT Tag        : ${process.env.GATSBY_DOC_BUILD_GIT_SHA}
+  Docs Staging   : ${process.env.GATSBY_DOC_BUILD_SUBMODULE_SHA}
   =====================================
   `);
 };
