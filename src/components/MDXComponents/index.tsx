d3b9805348f2bb1e42403d410053214946712d8e
Yuiham <Yuiham@users.noreply.github.com>
2024-09-10 15:24:06 +0800

    fix(mdx-components): correct export

--- src/components/MDXComponents/index.tsx
+++ src/components/MDXComponents/index.tsx
@@ -26,4 +26,4 @@ export { MDSvgIcon } from "components/MDXComponents/MDSvgIcon";
 export { SimpleTab } from "components/MDXComponents/SimpleTab";
 export { SyntaxDiagram } from "components/MDXComponents/SyntaxDiagram";
 export { TabsPanel } from "components/MDXComponents/TabsPanel";
-export { IntroBanner, IntroBannerVideo, DevLangAccordion, DevToolGroup, DevToolCard } from 'components/MDXComponents/developer'
+export { IntroHero, IntroHeroVideo, DevLangAccordion, DevToolGroup, DevToolCard } from 'components/MDXComponents/developer'
