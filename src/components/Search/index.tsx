28ee26f814f8553fc9e579bb4d81cd75ea18d6ba
Suhaha <jklopsdfw@gmail.com>
2024-11-20 17:19:33 +0800

    feat: support external search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -9,6 +9,8 @@ import IconButton from "@mui/material/IconButton";
 import { styled } from "@mui/material/styles";
 
 import SearchIcon from "@mui/icons-material/Search";
+import { Card, Menu, MenuItem, Popper, PopperProps } from "@mui/material";
+import { Locale } from "shared/interface";
 
 const StyledTextField = styled((props: TextFieldProps) => (
   <TextField {...props} />
@@ -35,9 +37,12 @@ export default function Search(props: {
 }) {
   const { placeholder, disableResponsive, docInfo } = props;
 
+  const anchorEl = React.useRef(null);
   const [queryStr, setQueryStr] = React.useState("");
+  const [isFocus, setIsFocus] = React.useState(false);
+  const [popperItemIndex, setPopperItemIndex] = React.useState(0);
 
-  const { t, navigate } = useI18next();
+  const { t, navigate, language } = useI18next();
   const theme = useTheme();
   const location = useLocation();
 
@@ -45,20 +50,82 @@ export default function Search(props: {
     setQueryStr(event.target.value);
   };
 
-  const handleSearchSubmitCallback = React.useCallback(() => {
-    navigate(
-      `/search?type=${docInfo.type}&version=${
-        docInfo.version
-      }&q=${encodeURIComponent(queryStr)}`,
-      {
-        state: {
-          type: docInfo.type,
-          version: docInfo.version,
-          query: queryStr,
-        },
-      }
-    );
-  }, [docInfo, queryStr]);
+  const handleSearchSubmitCallback = (forceKey?: string) => {
+    const key = forceKey || items[popperItemIndex].key;
+    const q = encodeURIComponent(queryStr);
+    if (key === "onsite") {
+      navigate(
+        `/search?type=${docInfo.type}&version=${docInfo.version}&q=${q}`,
+        {
+          state: {
+            type: docInfo.type,
+            version: docInfo.version,
+            query: queryStr,
+          },
+        }
+      );
+      return;
+    }
+    if (key === "google") {
+      window.open(
+        `https://www.google.com/search?q=site%3Adocs.pingcap.com+${q}`,
+        "_blank"
+      );
+    }
+    if (key === "bing") {
+      window.open(
+        `https://cn.bing.com/search?q=site%3Adocs.pingcap.com+${q}`,
+        "_blank"
+      );
+    }
+  };
+
+  const items: SearchPopperItem[] = React.useMemo(
+    () =>
+      (
+        [
+          {
+            key: "onsite",
+            component: ({ selected, query }) => (
+              <MenuItem
+                selected={selected}
+                onClick={() => handleSearchSubmitCallback("onsite")}
+                sx={{ textWrap: "auto" }}
+              >
+                {t("navbar.onsiteSearch")}: {query}
+              </MenuItem>
+            ),
+          },
+          {
+            key: "google",
+            component: ({ selected, query }) => (
+              <MenuItem
+                selected={selected}
+                onClick={() => handleSearchSubmitCallback("google")}
+                sx={{ textWrap: "auto" }}
+              >
+                Google: {query}
+              </MenuItem>
+            ),
+          },
+          {
+            key: "bing",
+            component: ({ selected, query }) => (
+              <MenuItem
+                selected={selected}
+                onClick={() => handleSearchSubmitCallback("bing")}
+                sx={{ textWrap: "auto" }}
+              >
+                Bing 搜索: {query}
+              </MenuItem>
+            ),
+          },
+        ] as SearchPopperItem[]
+      ).filter((item) =>
+        language === Locale.zh ? item.key !== "google" : item.key !== "bing"
+      ),
+    []
+  );
 
   React.useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
@@ -67,56 +134,112 @@ export default function Search(props: {
   }, [location.search]);
 
   return (
-    <Box>
-      {!disableResponsive && (
-        <IconButton
+    <>
+      <Box ref={anchorEl}>
+        {!disableResponsive && (
+          <IconButton
+            sx={{
+              display: {
+                lg: "none",
+              },
+            }}
+            onClick={handleSearchSubmitCallback}
+          >
+            <SearchIcon />
+          </IconButton>
+        )}
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
           sx={{
+            width: "251px",
             display: {
-              lg: "none",
+              xs: disableResponsive ? "block" : "none",
+              lg: "block",
             },
           }}
-          onClick={handleSearchSubmitCallback}
         >
-          <SearchIcon />
-        </IconButton>
-      )}
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
-        sx={{
-          width: "251px",
-          display: {
-            xs: disableResponsive ? "block" : "none",
-            lg: "block",
-          },
-        }}
-      >
-        <StyledTextField
-          size="small"
-          id="doc-search"
-          fullWidth
-          placeholder={t("navbar.searchDocs") || placeholder}
-          type="search"
-          variant="outlined"
-          value={queryStr}
-          onChange={handleChange}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              e.preventDefault();
-              handleSearchSubmitCallback();
-            }
-          }}
-          onSubmit={handleSearchSubmitCallback}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon fontSize="small" />
-              </InputAdornment>
-            ),
-          }}
-        />
+          <StyledTextField
+            size="small"
+            id="doc-search"
+            fullWidth
+            placeholder={t("navbar.searchDocs") || placeholder}
+            type="search"
+            variant="outlined"
+            value={queryStr}
+            onChange={handleChange}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                handleSearchSubmitCallback();
+              }
+              if (e.key === "ArrowUp") {
+                setPopperItemIndex((i) =>
+                  i - 1 < 0 ? items.length - 1 : i - 1
+                );
+              }
+              if (e.key === "ArrowDown") {
+                setPopperItemIndex((i) => (i + 1) % items.length);
+              }
+            }}
+            onSubmit={handleSearchSubmitCallback}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon fontSize="small" />
+                </InputAdornment>
+              ),
+            }}
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => setTimeout(() => setIsFocus(false), 100)}
+          />
+        </Box>
       </Box>
-    </Box>
+      <SearchPopper
+        open={!!queryStr && isFocus}
+        query={queryStr}
+        anchorEl={anchorEl.current}
+        popperItemIndex={popperItemIndex}
+        items={items}
+      />
+    </>
   );
 }
+
+interface SearchPopperItem {
+  key: string;
+  component: (props: {
+    selected: boolean;
+    query: string;
+  }) => React.ReactElement;
+}
+
+const SearchPopper = ({
+  open,
+  anchorEl,
+  popperItemIndex,
+  items,
+  query,
+}: PopperProps & {
+  query: string;
+  items: SearchPopperItem[];
+  popperItemIndex: number;
+}) => {
+  const { t } = useI18next();
+
+  return (
+    <Popper
+      open={open}
+      anchorEl={anchorEl}
+      sx={{ zIndex: 99 }}
+      modifiers={[{ name: "offset", options: { offset: [0, 8] } }]}
+    >
+      <Card sx={{ width: 251, wordBreak: "break-all" }}>
+        {items.map((item, index) => (
+          <item.component query={query} selected={popperItemIndex === index} />
+        ))}
+      </Card>
+    </Popper>
+  );
+};
