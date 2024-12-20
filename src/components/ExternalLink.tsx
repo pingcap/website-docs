04be8bf3301047605c0972837a9be539b446acfe
Suhaha <jklopsdfw@gmail.com>
2024-12-20 12:38:38 +0800

    chore: remove p tag outside link

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -2,14 +2,12 @@ import { Link } from "gatsby";
 
 export const ExternalLink = (props: { to: string; [key: string]: any }) => {
   return (
-    <p>
-      <Link
-        {...props}
-        onClick={(e) => {
-          e.preventDefault();
-          window.open((e.target as HTMLAnchorElement).href, "_blank");
-        }}
-      />
-    </p>
+    <Link
+      {...props}
+      onClick={(e) => {
+        e.preventDefault();
+        window.open((e.target as HTMLAnchorElement).href, "_blank");
+      }}
+    />
   );
 };
