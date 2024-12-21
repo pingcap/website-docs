0ec87f6bb23e18896caef0cce11ec1b57e8fec8a
Suhaha <jklopsdfw@gmail.com>
2024-12-21 14:44:05 +0800

    tweak: Link

--- src/components/MDXComponents/Link.tsx
+++ src/components/MDXComponents/Link.tsx
@@ -2,15 +2,13 @@ import { Link } from "gatsby";
 
 export const TargetLink = (props: { to: string; [key: string]: any }) => {
   const { to } = props;
-  const toArr = to.split("?");
-  const path = toArr[0];
-  const query = toArr[toArr.length - 1];
+  const { pathWithHash, query } = parsePathWithQuery(to);
   const newProps = { ...props };
-  const isTargetBlank = query.includes("target=_blank");
+  const isTargetBlank = query["target"] === "_blank";
 
   if (isTargetBlank) {
     newProps.target = "_blank";
-    newProps.to = path;
+    newProps.to = pathWithHash;
     newProps.rel = "noopener noreferrer";
   }
 
@@ -25,4 +23,40 @@ export const TargetLink = (props: { to: string; [key: string]: any }) => {
       }}
     />
   );
+};
+
+const parsePathWithQuery = (path: string) => {
+  const result: { query: { [k: string]: string }; pathWithHash: string } = {
+    query: {},
+    pathWithHash: "",
+  };
+
+  const [pathPart, hashPart] = path.split("#");
+
+  let actualPath = pathPart;
+  let hash = "";
+  let queryPart = "";
+
+  if (hashPart && hashPart.includes("?")) {
+    const [actualHash, query] = hashPart.split("?");
+    hash = actualHash;
+    queryPart = query;
+  } else {
+    hash = hashPart || "";
+
+    const isQueryInPath = pathPart.includes("?");
+    actualPath = isQueryInPath ? pathPart.split("?")[0] : pathPart;
+    queryPart = isQueryInPath ? pathPart.split("?")[1] : "";
+  }
+
+  if (queryPart) {
+    const params = new URLSearchParams(queryPart);
+    for (const [key, value] of params.entries()) {
+      result.query[key] = value;
+    }
+  }
+
+  result.pathWithHash = actualPath + (hash ? `#${hash}` : "");
+
+  return result;
 };
