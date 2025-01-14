59c157ece2742328e1f52a8e344a80093ef1d732
Suhaha <jklopsdfw@gmail.com>
2025-01-14 18:23:27 +0800

    fix: static bug (#578)
    * fix: static bug
    
    * update

--- src/components/Layout/Seo.tsx
+++ src/components/Layout/Seo.tsx
@@ -23,7 +23,7 @@ export default function Seo({
   link = [],
   archived = false,
 }: Props) {
-  const { site, favicon, favicon48 } = useStaticQuery(graphql`
+  const { site, favicon } = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
@@ -33,9 +33,6 @@ export default function Seo({
         }
       }
       favicon: file(relativePath: { eq: "favicon.ico" }) {
-        publicURL
-      }
-      favicon48: file(relativePath: { eq: "favicon-48x48.ico" }) {
         publicURL
       }
     }
@@ -126,11 +123,11 @@ export default function Seo({
         ...meta,
       ]}
       link={[
-        {
-          rel: "icon",
-          sizes: "48x48",
-          href: favicon48.publicURL,
-        },
+        // {
+        //   rel: "icon",
+        //   sizes: "48x48",
+        //   href: favicon48.publicURL,
+        // },
         {
           rel: "icon",
           sizes: "256x256",
