5ca08e0e8f8db3dd3f13f691c0b76c0024effdf8
Suhaha <jklopsdfw@gmail.com>
2026-02-11 11:25:23 +0800

    feat(banner): auto translation zh ai

--- src/shared/useIsAutoTranslation.ts
+++ src/shared/useIsAutoTranslation.ts
@@ -3,9 +3,13 @@ import { TOCNamespace } from "shared/interface";
 
 export const useIsAutoTranslation = (namespace: TOCNamespace) => {
   const { language } = useI18next();
+  const isJA = language === "ja";
+  const isZH = language === "zh";
   const isAutoTranslation =
     namespace !== TOCNamespace.Home &&
-    (language === "ja" ||
-      (language === "zh" && namespace === TOCNamespace.TiDBCloud));
+    (isJA ||
+      (isZH &&
+        (namespace === TOCNamespace.TiDBCloud ||
+          namespace === TOCNamespace.AI)));
   return isAutoTranslation;
 };
