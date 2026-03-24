6144a60d59bc8d73a7f49ac78b193c2c699014d3
qiancai <qqzczy@126.com>
2026-03-24 16:18:07 +0800

    feat(tooltip): support tap interaction on touch
    Add touch-device handling for glossary tooltips so tap toggles the tooltip and click-away closes it, while desktop hover and keyboard focus behavior stay unchanged.

--- src/components/MDXComponents/Tooltip.tsx
+++ src/components/MDXComponents/Tooltip.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 import MuiTooltip from "@mui/material/Tooltip";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { graphql, useStaticQuery } from "gatsby";
 import { useI18next } from "gatsby-plugin-react-i18next";
 
@@ -52,6 +54,10 @@ export const Tooltip = (props: {
 }) => {
   const { id, children } = props;
   const { language } = useI18next();
+  const isTouchDevice = useMediaQuery("(hover: none) and (pointer: coarse)", {
+    noSsr: true,
+  });
+  const [isTouchTooltipOpen, setIsTouchTooltipOpen] = React.useState(false);
   const data = useStaticQuery<TooltipQueryData>(graphql`
     query TooltipTermDefinitions {
       allTooltipTerm {
@@ -79,64 +85,105 @@ export const Tooltip = (props: {
     return <>{children}</>;
   }
 
+  const handleTouchTooltipToggle = (
+    event: React.MouseEvent<HTMLSpanElement>
+  ) => {
+    if (!isTouchDevice) {
+      return;
+    }
+
+    event.preventDefault();
+    setIsTouchTooltipOpen((open) => !open);
+  };
+
+  const handleTouchTooltipKeyDown = (
+    event: React.KeyboardEvent<HTMLSpanElement>
+  ) => {
+    if (event.key === "Escape") {
+      setIsTouchTooltipOpen(false);
+      return;
+    }
+
+    if (!isTouchDevice || (event.key !== "Enter" && event.key !== " ")) {
+      return;
+    }
+
+    event.preventDefault();
+    setIsTouchTooltipOpen((open) => !open);
+  };
+
+  const handleTouchTooltipClose = () => {
+    setIsTouchTooltipOpen(false);
+  };
+
   return (
-    <MuiTooltip
-      title={localizedDefinition}
-      arrow
-      enterDelay={100}
-      placement="top"
-      disableTouchListener
-      componentsProps={{
-        popper: {
-          modifiers: [
-            {
-              name: "offset",
-              options: {
-                offset: [0, -6],
-              },
+    <ClickAwayListener onClickAway={handleTouchTooltipClose}>
+      <Box component="span" sx={{ display: "inline" }}>
+        <MuiTooltip
+          title={localizedDefinition}
+          arrow
+          enterDelay={100}
+          placement="top"
+          open={isTouchDevice ? isTouchTooltipOpen : undefined}
+          disableHoverListener={isTouchDevice}
+          disableFocusListener={isTouchDevice}
+          disableTouchListener
+          onClose={handleTouchTooltipClose}
+          componentsProps={{
+            popper: {
+              modifiers: [
+                {
+                  name: "offset",
+                  options: {
+                    offset: [0, -6],
+                  },
+                },
+              ],
             },
-          ],
-        },
-        tooltip: {
-          sx: (theme) => ({
-            backgroundColor: theme.palette.carbon[800],
-            color: theme.palette.carbon[50],
-            fontFamily: theme.typography.fontFamily,
-            fontSize: theme.typography.body2.fontSize,
-            fontWeight: theme.typography.body2.fontWeight,
-            fontStyle: theme.typography.body2.fontStyle,
-            lineHeight: theme.typography.body2.lineHeight,
-            borderRadius: "6px",
-          }),
-        },
-        arrow: {
-          sx: (theme) => ({
-            color: theme.palette.carbon[800],
-          }),
-        },
-      }}
-    >
-      <Box
-        component="span"
-        tabIndex={0}
-        sx={(theme) => ({
-          textDecorationLine: "underline",
-          textDecorationStyle: "dotted",
-          textDecorationColor: theme.palette.carbon[700],
-          textDecorationThickness: "1px",
-          textUnderlineOffset: "4px",
-          outline: "none",
-          fontVariantLigatures: "none",
-          "&:focus-visible": {
-            outline: `2px solid ${theme.palette.carbon[700]}`,
-            outlineOffset: "2px",
-            textDecorationThickness: "2px",
-            textDecorationColor: theme.palette.carbon[900],
-          },
-        })}
-      >
-        {children}
+            tooltip: {
+              sx: (theme) => ({
+                backgroundColor: theme.palette.carbon[800],
+                color: theme.palette.carbon[50],
+                fontFamily: theme.typography.fontFamily,
+                fontSize: theme.typography.body2.fontSize,
+                fontWeight: theme.typography.body2.fontWeight,
+                fontStyle: theme.typography.body2.fontStyle,
+                lineHeight: theme.typography.body2.lineHeight,
+                borderRadius: "6px",
+              }),
+            },
+            arrow: {
+              sx: (theme) => ({
+                color: theme.palette.carbon[800],
+              }),
+            },
+          }}
+        >
+          <Box
+            component="span"
+            tabIndex={0}
+            onClick={handleTouchTooltipToggle}
+            onKeyDown={handleTouchTooltipKeyDown}
+            sx={(theme) => ({
+              textDecorationLine: "underline",
+              textDecorationStyle: "dotted",
+              textDecorationColor: theme.palette.carbon[700],
+              textDecorationThickness: "1px",
+              textUnderlineOffset: "4px",
+              outline: "none",
+              fontVariantLigatures: "none",
+              "&:focus-visible": {
+                outline: `2px solid ${theme.palette.carbon[700]}`,
+                outlineOffset: "2px",
+                textDecorationThickness: "2px",
+                textDecorationColor: theme.palette.carbon[900],
+              },
+            })}
+          >
+            {children}
+          </Box>
+        </MuiTooltip>
       </Box>
-    </MuiTooltip>
+    </ClickAwayListener>
   );
 };
