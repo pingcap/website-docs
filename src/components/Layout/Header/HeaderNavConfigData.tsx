da7249c202082414769f8a2b66c425f9b963e734
qiancai <qqzczy@126.com>
2026-08-05 16:01:12 +0800

    Update HeaderNavConfigData.tsx

--- src/components/Layout/Header/HeaderNavConfigData.tsx
+++ src/components/Layout/Header/HeaderNavConfigData.tsx
@@ -86,6 +86,7 @@ const getDefaultNavConfig = (
             endIcon: <PreviewBadge label={t("navbar.badge.preview")} />,
             to: "/tidbcloudlake",
             selected: (namespace) => namespace === TOCNamespace.TiDBCloudLake,
+            disabled: (lang: string) => lang === "zh" || lang === "ja",
             onClick: () => {
               if (typeof window !== "undefined") {
                 sessionStorage.setItem(CLOUD_MODE_KEY, CloudPlan.Dedicated);
